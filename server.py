36c7f9e6226098538f8e44972f6f08ba568ec119
Wilton Nunes Gomes da Silva <will_nunnes@hotmail.com>
2021-06-20 00:03:29 -0300

    Alterar server.py

--- server.py
+++ server.py
@@ -1,5 +1,28 @@
 from socket import socket
 import argparse
+from threading import Thread
+
+class ClientTh(Thread):
+
+  def __ini__(self, sock):
+    Thread.__init__(self)
+    self.sock = sock
+
+  def run(self):
+    while True:
+      data = self.sock.recv(4096)
+      print(data)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("host", default = '')
+parser.add_argument("port", default = 50007, type = integer)
+args = parser.parse_args()
 
 sock = socket()
-sock.bind()
+sock.bind((args.host, args.port))
+sock.listen()
+while True:
+	conn, addr = sock.accept()
+  print(addr)
+  clientTh = ClientTh(conn)
+  clientTh.start()
